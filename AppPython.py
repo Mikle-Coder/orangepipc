5e04b5eb72717cbf02112cb2e2f081a1e38e6bb3
Mikle-Coder <89573920+Mikle-Coder@users.noreply.github.com>
2022-11-28 00:39:58 +0300

    Add AppPython

---
+++ AppPython.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from tkinter import messagebox
+ 
+window = Tk()
+window.title('Тестовое приложение')
+window.geometry('150x80')
+window.eval('tk::PlaceWindow . center')
+
+frame = Frame(
+   window,
+   padx=10,
+   pady=10
+)
+frame.pack(expand=True)
+
+textArray = [
+   "Привет, Олег",
+   "Пока, Олег",
+   "Я сказал ПОКА, Олег",
+   "Закрыть приложение"
+   ]
+   
+indexOfArray = 0
+
+def calculate_bmi():
+   global indexOfArray
+   if indexOfArray == len(textArray) - 1:
+      window.destroy()
+   else:
+      indexOfArray = indexOfArray + 1
+      cal_btn.configure(text=textArray[indexOfArray])
+   
+ 
+cal_btn = Button(
+   frame,
+   text=textArray[indexOfArray],
+   command=calculate_bmi
+)
+
+cal_btn.grid(row=5, column=2)
+ 
+window.mainloop()
